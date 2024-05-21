7e37c36ab0e59d39ee039c96709e1594fcaff997
RyanByers1023 <ryanbyers1023@gmail.com>
2024-05-20 20:26:32 -0500

    Create Layer.py

---
+++ Layer.py
@@ -0,0 +1,9 @@
+import Neuron
+
+#if a network is looked at as a matrix:
+#The entire layer is a column, numNeurons indicates # of rows, numInputs indicates num of rows from previous column.
+class Layer:
+    def __init__(self, numNeurons, numInputs):
+        #create a column of neurons
+        self.neurons = [Neuron(numInputs) for _ in range(numNeurons)]
+    
