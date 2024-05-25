d9c217b54d32ab1f5e462bab344a751ddffbfe82
RyanByers1023 <ryanbyers1023@gmail.com>
2024-05-25 18:33:14 -0500

    Add CalculateLossFunction() and CalculateLossFunctionDerivative()

--- Layer.py
+++ Layer.py
@@ -1,9 +1,11 @@
 import Neuron
 
 #if a network is looked at as a matrix:
-#The entire layer is a column, numNeurons indicates # of rows, numInputs indicates num of rows from previous column.
+#The entire layer is a column, numNeurons indicates # of rows, numInputs indicates num of rows from previous column (layer).
 class Layer:
     def __init__(self, numNeurons, numInputs):
         #create a column of neurons
+        self.numNeurons = numNeurons
         self.neurons = [Neuron(numInputs) for _ in range(numNeurons)]
     #implement forward propagation operation:
+
--- LossFunctions.py
+++ LossFunctions.py
@@ -1,9 +1,33 @@
 #I will implement a loss function called: "MSE" "Mean Squared Error"
 #L = 1/n * summation from 1 to n of((y-hat - y)^2)
-#1/2 can be substituted for 1/n for simplicity in the calculation of the derivative
+#n represents the number of neurons in the current layer
+#1/2 can be substituted for 1/n for simplicity in the calculation of the derivative.
 #y-hat = x * weight
 #x represents the raw data that was streamed in to this particular neuron
 #y is representative of the target value
-#n represents the number of neurons in the output layer
 
 #this function calculates a value called loss, which represents how far off the output is from the expected output y
+#with this in mind, MSE is the function that needs to be minimized
+
+import numpy
+
+#MSE loss function:
+def CalculateLossFunction(trueLayer, predictedLayer):
+    #convert layers to numpy arrays
+    trueLayer = numpy.array(trueLayer)
+    predictedLayer = numpy.array(predictedLayer)
+
+    #element wise operations for (y-hat - y)^2
+    squareDifference = (trueLayer - predictedLayer) ** 2
+
+    mse = numpy.mean(squareDifference)
+
+    return mse
+
+def CalculateLossFunctionDerivative(trueLayer, predictedLayer):
+    trueLayer = numpy.array(trueLayer)
+    predictedLayer = numpy.array(predictedLayer)
+
+    derivative = 2 * (predictedLayer - trueLayer) / trueLayer.numNeurons
+
+    return derivative
