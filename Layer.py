177cdfd0dc1c4dd9a1ac694c6a5693abff62fc73
RyanByers1023 <ryanbyers1023@gmail.com>
2024-05-26 17:29:01 -0500

    Begin defintion for __init__ function for layer object

--- Layer.py
+++ Layer.py
@@ -1,11 +1,24 @@
 import Neuron
+import numpy
 
 #if a network is looked at as a matrix:
 #The entire layer is a column, numNeurons indicates # of rows, numInputs indicates num of rows from previous column (layer).
 class Layer:
-    def __init__(self, numNeurons, numInputs):
-        #create a column of neurons
-        self.numNeurons = numNeurons
-        self.neurons = [Neuron(numInputs) for _ in range(numNeurons)]
+    #inputNumNeurons references the number of neurons from the previous layer
+    #numNeurons references the number of neurons from the current layer
+    def __init__(self, inputNumNeurons, numNeurons):
+        #seed random number generator
+        numpy.random.seed(71)
+        #below scalar is used to ensure values that the neurons are intialized with are small
+        scalar = 0.01
+        #parameters dictionary will be used to store both the weight and the bias for the corresponding neuron
+        #how to determine which parameter entry goes with which neuron?
+        #the indices tell you: parameter[W0] goes with layer[0] and so on...
+
+        parameters = {}
+        #initialize parameters
+        for neuronIndex in range(1, numNeurons):
+            parameters['W' + str(neuronIndex)] = numpy.random.randn(numNeurons, 1) * scalar
+            parameters['b' + str(neuronIndex)]
     #implement forward propagation operation:
 
