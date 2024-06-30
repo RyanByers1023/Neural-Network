28e26c52b18b3d3e2c8a4f3cbc926988841f7069
RyanByers1023 <ryanbyers1023@gmail.com>
2024-06-30 12:25:19 -0500

    MAINTENENCE: change/remove comments to be more concise. fix syntax error in parameter declaration

--- Layer.py
+++ Layer.py
@@ -2,23 +2,23 @@ import Neuron
 import numpy
 
 #if a network is looked at as a matrix:
-#The entire layer is a column, numNeurons indicates # of rows, numInputs indicates num of rows from previous column (layer).
-class Layer:
+#this object represents a single column of the matrix that makes up the neural network
     #inputNumNeurons references the number of neurons from the previous layer
-    #numNeurons references the number of neurons from the current layer
     def __init__(self, inputNumNeurons, numNeurons):
         #seed random number generator
         numpy.random.seed(71)
         #below scalar is used to ensure values that the neurons are intialized with are small
         scalar = 0.01
-        #parameters dictionary will be used to store both the weight and the bias for the corresponding neuron
-        #how to determine which parameter entry goes with which neuron?
-        #the indices tell you: parameter[W0] goes with layer[0] and so on...
 
-        parameters = {}
+        #parameters used to store both the weight and the bias for the corresponding neuron
+        #W0 = first 
+        self.parameters = {}
         #initialize parameters
         for neuronIndex in range(1, numNeurons):
+            #initialize weights
             parameters['W' + str(neuronIndex)] = numpy.random.randn(numNeurons, 1) * scalar
+            #initialize biases
             parameters['b' + str(neuronIndex)]
+
     #implement forward propagation operation:
 
